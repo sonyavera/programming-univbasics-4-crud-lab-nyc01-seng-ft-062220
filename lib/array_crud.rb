7289c9e67c899b5cc9d98e18381834f19e0e8154
Sonya Gould <sonyavgould@gmail.com>
2020-06-05 22:32:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  Array.new = ["bananes", 3, true, "goats"]
+  arr = ["bananes", 3, true, "goats"]
 end
 
 def add_element_to_end_of_array(array, element)
